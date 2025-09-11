ab3072f11de75d4c61bbf608681fea6553cb26a3
Danielcc2 <cabreracoellodaniel@gmail.com>
2025-09-11 15:30:58 +0100

    config: set Supabase URL and anon key

--- config/supabase.js
+++ config/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-export const SUPABASE_URL = 'https://TU-PROYECTO.supabase.co';
-export const SUPABASE_ANON_KEY = 'TU_ANON_KEY';
+export const SUPABASE_URL = 'https://nvrjdmhgginfywmdpxtb.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52cmpkbWhnZ2luZnl3bWRweHRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1OTgwNjAsImV4cCI6MjA3MzE3NDA2MH0.TrtPEe9YXawMzPx1MgpGhDoYPdxDbSxseTZHEC-lZn8';
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, { auth: { persistSession: true, autoRefreshToken: true } });
